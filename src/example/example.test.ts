1b84ea76dbd16b34fd3dcce3f4ecab0364895923
Michael Gee <michaelgee221@gmail.com>
2023-12-26 10:44:16 -0600

    add isNil function with tests and remove example util from build bundle

--- src/example/example.test.ts
+++ src/example/example.test.ts
@@ -1,8 +1,11 @@
 import { describe, it, expect } from "vitest";
 import { addTwoNumbers } from "./example";
 
-describe("example utils", () => {
-  it("addTwoNumbers() funciton adds 2 + 2 to equal and return 4", () => {
+describe("addTwoNumbers()", () => {
+  it("should return the sum of two numbers", () => {
     expect(addTwoNumbers(2, 2)).toBe(4);
+    expect(addTwoNumbers(5, 5)).toBe(10);
+    expect(addTwoNumbers(50, 50)).toBe(100);
+    expect(addTwoNumbers(2234, 4000)).toBe(6234);
   });
 });
---
+++ src/isNil/isNil.test.ts
@@ -0,0 +1,17 @@
+import { describe, it, expect } from "vitest";
+import { isNil } from "./isNil";
+
+describe("isNil()", () => {
+  it("should specify if the parameter passed is `null` or `undefined", () => {
+    expect(isNil(true)).toBe(false);
+    expect(isNil(false)).toBe(false);
+    expect(isNil("string")).toBe(false);
+    expect(isNil(22)).toBe(false);
+    expect(isNil(0)).toBe(false);
+    expect(isNil({})).toBe(false);
+    expect(isNil([])).toBe(false);
+
+    expect(isNil(undefined)).toBe(true);
+    expect(isNil(null)).toBe(true);
+  });
+});
