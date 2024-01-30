6ad2fc3d504c4473b7e1c5eb1c184d40ccd4b7ee
Michael Gee <michaelgee221@gmail.com>
2024-01-29 23:44:24 -0500

    add eslint with default TypeScript rules - add eslint --fix command to lint-staged to run on every commit

--- src/omit/omit.ts
+++ src/omit/omit.ts
@@ -18,6 +18,7 @@ export function omit(object: Record<string, unknown>, properties: string | strin
     return copyObject;
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { [properties]: _, ...rest } = object;
   return rest;
 }
