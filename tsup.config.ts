0a21bdf6790915d58f25ecdf2be98b99189825a9
Michael Gee <michaelgee221@gmail.com>
2025-08-08 23:15:26 -0400

    migrate to tsdown

--- tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['esm', 'cjs'], // Output both ESM and CJS entry: ['src/**/*.ts'], // Entry points
-  minify: true,
-  clean: true,
-  dts: true
-});
